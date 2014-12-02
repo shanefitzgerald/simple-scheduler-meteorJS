c6391fecbff649c438f328328fe1b4c8a4dc7799
GoodDoctor05 <shanesrf@gmail.com>
2014-12-02 15:14:23 -0800

    first commit

---
+++ simple-scheduler.js
@@ -0,0 +1,37 @@
+if (Meteor.isClient) {
+  // This code only runs on the client
+  Template.body.helpers({
+    instructorOneSchedules: [
+      { instructor: "Shane",
+        date: "Monday December 8th",
+        blockOne: "7:00-7:30",
+        blockTwo: "7:30-8:00",
+        blockThree: "8:00-8:30",
+        blockFour: "8:30-9:00"  
+      },
+      { instructor: "Shane",
+        date: "Thursday December 9th",
+        blockOne: "7:00-7:30",
+        blockTwo: "7:30-8:00",
+        blockThree: "8:00-8:30",
+        blockFour: "8:30-9:00"  
+      }
+    ],
+    instructorTwoSchedules: [
+      { instructor: "Kameron",
+        date: "Tuesday December 9th",
+        blockOne: "7:00-7:30",
+        blockTwo: "7:30-8:00",
+        blockThree: "8:00-8:30",
+        blockFour: "8:30-9:00"  
+      },
+      { instructor: "Kameron",
+        date: "Friday December 9th",
+        blockOne: "7:00-7:30",
+        blockTwo: "7:30-8:00",
+        blockThree: "8:00-8:30",
+        blockFour: "8:30-9:00"  
+      }
+    ]
+  });
+}
